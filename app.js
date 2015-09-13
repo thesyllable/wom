374bfa11e1db4fc6c963ad36e686735a2233c4da
thesyllable <etocampo@yahoo.com>
2015-09-12 22:16:56 -0700

    install express

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Welcome to WOM!');
+});
+
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('WOM is listening at http://%s:%s', host, port);
+});
